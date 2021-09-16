e6e53c4e232c6e89257f5095553d4fadaf8690dc
magehrig <mathias.gehrig@gmail.com>
2021-09-16 16:34:37 +0200

    create tensors on device

--- core/raft.py
+++ core/raft.py
@@ -63,8 +63,8 @@ class RAFT(nn.Module):
     def initialize_flow(self, img):
         """ Flow is represented as difference between two coordinate grids flow = coords1 - coords0"""
         N, C, H, W = img.shape
-        coords0 = coords_grid(N, H//8, W//8).to(img.device)
-        coords1 = coords_grid(N, H//8, W//8).to(img.device)
+        coords0 = coords_grid(N, H//8, W//8, device=img.device)
+        coords1 = coords_grid(N, H//8, W//8, device=img.device)
 
         # optical flow computed as difference: flow = coords1 - coords0
         return coords0, coords1
